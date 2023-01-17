d85881b3f009abc0418806560bc812f56173798e
anakon73 <nazarkinah73@gmail.com>
2023-01-17 18:39:31 +0200

    feat(app): added archive func and filtered by unarchived emails

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { type email } from '@/types/emailType'
 import { ref, computed } from 'vue'
+import { format } from 'date-fns'
 
 const emails = ref<email[]>([
   {
@@ -63,6 +64,13 @@ const mailClasses = computed(() => {
     email.read ? 'bg-zinc-100' : '',
   ]
 })
+
+const sortedEmails = computed(() => {
+  return emails.value.sort((el1, el2) => (el1.sentAt < el2.sentAt ? 1 : -1))
+})
+const unarchivedEmails = computed(() => {
+  return sortedEmails.value.filter((e) => !e.archived)
+})
 </script>
 
 <template>
@@ -71,26 +79,33 @@ const mailClasses = computed(() => {
   <table class="max-w-[62.5rem] m-auto border-collapse">
     <tbody>
       <tr
-        v-for="email in emails"
+        v-for="email in unarchivedEmails"
         :key="email.id"
         :class="mailClasses(email)"
         @click="email.read = true"
       >
-        <td class="border-b border-t border-black p-1 text-left">
+        <td class="border-b border-t border-black p-2 text-left">
           <input type="checkbox" :class="checkboxClasses" />
         </td>
         <td class="border-b border-t border-black p-1 text-left">
           {{ email.from }}
         </td>
         <td class="border-b border-t border-black p-1 text-left">
-          <p class="max-h-[1.2rem] overflow-y-hidden m-0">
-            <span class="font-bold"
-              >{{ email.subject }} - {{ email.body }}</span
-            >
+          <p class="max-h-[1.4rem] overflow-y-hidden m-0">
+            <span class="font-bold">{{ email.subject }} - </span
+            >{{ email.body }}
           </p>
         </td>
         <td class="w-[7.5rem] border-b border-t border-black p-1 text-left">
-          {{ email.sentAt }}
+          {{ format(new Date(email.sentAt), 'MMM do yyyy') }}
+        </td>
+        <td class="border-b border-t border-black p-1 text-left">
+          <button
+            @click="email.archived = true"
+            class="p-2 rounded mt-[5px] mr-2.5 mb-[5px] border bg-slate-100"
+          >
+            Archive
+          </button>
         </td>
       </tr>
     </tbody>

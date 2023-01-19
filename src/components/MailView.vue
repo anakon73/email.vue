61b7788c33f3e5a0a911890f37831d3784f151d1
anakon73 <nazarkinah73@gmail.com>
2023-01-19 22:00:24 +0200

    feat(modalview): create toggle read and archive funcs

--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { type email } from '@/types/emailType'
+import { onKeyDown } from '@vueuse/core'
+import axios from 'axios'
 import { format } from 'date-fns'
 import { marked } from 'marked'
 import { computed, toRefs } from 'vue'
@@ -13,10 +15,46 @@ const { email } = toRefs(props)
 const markdownToHTML = computed(() => {
   return marked(email.value.body)
 })
+
+const toggleRead = () => {
+  email.value.read = !email.value.read
+  axios.put(`http://localhost:3000/emails/${email.value.id}`, email)
+}
+const toggleArchive = () => {
+  email.value.archived = !email.value.archived
+  axios.put(`http://localhost:3000/emails/${email.value.id}`, email)
+}
+
+const buttonClasses = [
+  'p-2',
+  'rounded',
+  'mt-[5px]',
+  'mr-2.5',
+  'mb-[5px]',
+  'border',
+  'bg-slate-100',
+]
+
+onKeyDown(['R', 'r'], () => {
+  toggleRead()
+})
+onKeyDown(['E', 'e'], () => {
+  toggleArchive()
+})
 </script>
 
 <template>
-  <div class="text-left mt-4">
+  <div class="text-left">
+    <div>
+      <button @click="toggleArchive" :class="buttonClasses">
+        {{ email.archived ? 'Move to Inbox (e)' : 'Archive (e)' }}
+      </button>
+      <button @click="toggleRead" :class="buttonClasses">
+        {{ email.read ? 'Mark unread (r)' : 'Mark Read (r)' }}
+      </button>
+      <button @click="goNewer" :class="buttonClasses">Newer (k)</button>
+      <button @click="goOlder" :class="buttonClasses">Older (j)</button>
+    </div>
     <h2 class="mb-0 text-2xl font-extrabold">
       Subject: <span class="font-bold">{{ email.subject }}</span>
     </h2>
